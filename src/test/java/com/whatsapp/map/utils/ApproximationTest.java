e533bdb766b1f5eff0ccca439e0ffbb36e1009d4
Aziz Namazov <azee@77.88.2.103-spb.dhcp.yndx.net>
2015-01-27 18:09:43 +0300

    Add tests

--- src/test/java/com/whatsapp/map/utils/ApproximationTest.java
+++ src/test/java/com/whatsapp/map/utils/ApproximationTest.java
@@ -1,14 +1,11 @@
 package com.whatsapp.map.utils;
 
-import ch.lambdaj.Lambda;
 import com.whatsapp.map.beans.Point;
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -25,20 +22,45 @@ public class ApproximationTest {
         input.add(new Point(10, 12.33));
 
         List<Point> result = Approximation.reduceThresholdDestination(input, 1);
+        assertThat(result, new PointListMatcher(input));
+    }
 
-        assertThat(result.size(), is(4));
+    @Test
+    public void reduceThresholdDestinationTest(){
+        List<Point> input = new LinkedList<Point>();
+        input.add(new Point(1, 1));
+        input.add(new Point(2, 2));
+        input.add(new Point(3, 2));
+        input.add(new Point(4, 3));
+        input.add(new Point(7, 7));
+        input.add(new Point(8, 8));
+        input.add(new Point(15, 15));
 
-        assertThat(Lambda.extract(input, Lambda.on(Point.class).getX()),
-                (Matcher) contains(
-                        Lambda.extract(result, Lambda.on(Point.class).getX())
-                )
-        );
+        List<Point> expexted = new LinkedList<Point>();
+        expexted.add(new Point(2.5, 2));
+        expexted.add(new Point(7.5, 7.5));
+        expexted.add(new Point(15, 15));
 
-        assertThat(Lambda.extract(input, Lambda.on(Point.class).getY()),
-                (Matcher) contains(
-                        Lambda.extract(result, Lambda.on(Point.class).getY())
-                )
-        );
+        List<Point> result = Approximation.reduceThresholdDestination(input, 6);
+        assertThat(result, new PointListMatcher(expexted));
+    }
+
+    @Test
+    public void reduceThresholdDestinationEmptyListTest(){
+        List<Point> input = new LinkedList<Point>();
+        List<Point> result = Approximation.reduceThresholdDestination(input, 1);
+        assertThat(result, new PointListMatcher(new ArrayList<Point>()));
+    }
+
+    @Test
+    public void reduceThresholdDestinationZeroTHTest(){
+        List<Point> input = new LinkedList<Point>();
+        input.add(new Point(1, 1));
+        input.add(new Point(2, 2));
+        input.add(new Point(3, 2));
+
+        List<Point> result = Approximation.reduceThresholdDestination(input, 0);
+        assertThat(result, new PointListMatcher(input));
     }
 
 }
--- src/test/java/com/whatsapp/map/utils/TestUtils.java
+++
@@ -1,8 +0,0 @@
-package com.whatsapp.map.utils;
-
-/**
- * Created by azee on 27.01.15.
- */
-public class TestUtils {
-
-}
